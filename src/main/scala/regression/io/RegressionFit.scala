eccbb5ba1d9e0d31010659aa810752dbd6cc871b
tipe <tiitus.jarvinen@aalto.fi>
2021-04-24 19:04:35 +0300

    Added some comments to RegressionFit

--- src/main/scala/regression/io/RegressionFit.scala
+++ src/main/scala/regression/io/RegressionFit.scala
@@ -3,7 +3,10 @@ package regression.io
 import scalafx.scene.paint.Color
 
 abstract class RegressionFit(val color: Color = Color.DarkBlue) {
+  /// Calculate the given x value for the polynomial y-value of this fit
   def polynomialValue(x: Double) : Double
+
+  /// Format this expression to a readable format (forex. -2x+3)
   def formattedExpression : String
 
   protected def coefFormat(coef: Double) : String = if (coef < 0) s"- ${coef.toString.substring(1)}" else s"+ $coef"
