d84f36c30131442f44cce23ac1d2c332c9c1e867
tipe <tiitus.jarvinen@aalto.fi>
2021-04-24 18:55:16 +0300

    Reformatted LinearFit

--- src/main/scala/regression/io/LinearFit.scala
+++ src/main/scala/regression/io/LinearFit.scala
@@ -6,16 +6,16 @@ class LinearFit(private val data : Seq[(Double, Double)], c: Color = Color.Alice
 
   private def leastSquaresFit(data: Seq[(Double, Double)]) : (Double, Double) = {
     val n = data.length
-    var vX = data.map(_._1).reduce((p, p2) => p + p2)/n
-    var vY = data.map(_._2).reduce((p, p2) => p + p2)/n
-    var sXX = 0.0
-    var sXY = 0.0
+    val vX = data.map(_._1).reduce((p, p2) => p + p2)/n // AVG of x-values
+    val vY = data.map(_._2).reduce((p, p2) => p + p2)/n // AVG of y-values
+    var sXX = 0.0 // Sum of x-AVG(x) squared
+    var sXY = 0.0 // Sum of (x-AVG(x))(y-AVG(y))
     for (p <- data) {
       sXY += (p._1 - vX) * (p._2 - vY)
       sXX += math.pow(p._1 - vX, 2)
     }
-    var m = sXY/sXX
-    var b = vY - m*vX
+    val m = sXY/sXX
+    val b = vY - m*vX
     (m, b)
   }
 
